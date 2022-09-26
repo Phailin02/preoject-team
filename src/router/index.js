79e30810f8db4db97576d3463423abea639cdb89
Annie Thammavongsa <Annythammavongsa@gmail.com>
2022-09-26 15:36:15 +0700

    rout

--- src/router/index.js
+++ src/router/index.js
@@ -36,11 +36,7 @@ const routes = [
     name: 'contact',
     component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
   },
-  {
-    path: '/aboutus',
-    name: 'aboutus',
-    component: () => import('../views/Aboutus.vue')
-  }
+
 ]
 
 const router = new VueRouter({
