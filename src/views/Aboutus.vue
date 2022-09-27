462e8903d4d81dca0cab1cb503995100055a2f3f
Phailin02 <phalinkhodyotha02.com>
2022-09-27 14:15:01 +0700

    updateaboutus

--- src/views/Aboutus.vue
+++ src/views/Aboutus.vue
@@ -1,44 +1,142 @@
 <template>
-    <v-app>
-        <div>
-        <v-main> 
-  
-            <v-container style="background-color: #EBEBEB">
-                <div>
-    
-               
-                        <v-card 
-    tile class="d-inline-flex pa-2 mx-auto px-15 mb-2" color="#297FCA" >
+  <v-app>
+    <div>
+      <v-main>
+        <v-container style="background-color: #ebebeb">
+          <div>
+            <v-card
+              tile
+              class="d-inline-flex  mx-auto px-15 mb-2 mt-10"
+              color="#297FCA"
+            >
+              <h1 class="white--text mb-1" style="font-weight: 500">
+                 Foundation of the Deaf
+              </h1>
+            </v-card>
+          </div>
+          <div class="pl-15 pr-15 pt-5  ma-10">
+            <v-row class="pb-10">
+              <v-col cols="6">
+                <v-img src="../assets/aboutus1.png"> </v-img>
+              </v-col>
+              <v-col class="pl-10" style="color:#5B5B5B">
+                <p>
+                  ມີຫຼາຍວິທີທີ່ຈະເບິ່ງແຜ່ນແພ:ເປັນພາສາທົ່ວໄປ; ປະຫວັດສາດຂອງມະ
+                  ນຸດຊາດແລະປະຫວັດສາດໃນການສ້າງ; ຄວາມຈໍາເປັນແລະຄວາມຫລູ
+                  ຫລາ;ຄວາມກັງວົນສ່ວນບຸກຄົນແລະອຸດສາຫະກໍາທົ່ວໂລກ; ບາງສິ່ງບາງ
+                  ຢ່າງທີ່ເຄີຍພັດທະນາ.</p>
+                <p> ຫຼັກສູດOpenFoundation
+                  ນີ້ຈະຊ່ວຍໃຫ້ທ່ານພັດທະນາທັກສະການ
+                  ເບິ່ງເຫັນຂອງທ່ານແລະຄົ້ນຫາວິທີການຕ່າງໆໃນການນໍາໃຊ້ວັດສະດຸແລະເຄື່ອງມືເພື່ອພັດທະນາແນວຄວາມຄິດສິ່ງທໍຂອງທ່ານເອງ.</p>
+                  <p>ເມື່ອທ່ານໄດ້ຮັບການແນະ ນຳ ກ່ຽວກັບລັກສະນະແລະສະພາບການຂອງ
+                  ແຜ່ນແພ,ທ່ານຈະສືບຕໍ່ສ້າງເຕັກນິກການສ້າງເຄື່ອງໝາຍ, ຄົ້ນຫາຮູບຊົງ
+                  ເປັນຮູບແຕ້ມແບບສັງເກດການສ້າງສັນສຳລັບວຽກງານແຜ່ນແພ, ຄົ້ນພົບ
+                  ຄຸນລັກສະນະຂອງວັດສະດຸແລະຂະຫຍາຍຄຳສັບສາຍຕາຂອງທ່ານ. ໂດຍ
+                  ຜ່ານການແຕ້ມຮູບ, collage ແລະເຕັກນິກການຫມູນໃຊ້.</p>
+                  <p>ເມື່ອສໍາເລັດຫ້າພາກສ່ວນຂອງຫຼັກສູດເປີດ, ທ່ານຄວນຈະໄດ້ຮັບທັກສະ
+                  ພຽງພໍແລະຄວາມຫມັ້ນໃຈພຽງພໍທີ່ຈະໄປຮຽນຕໍ່ກັບວິທະຍາໄລສິລະປະ ເປີດ,
+                  ແລະສາມາດຕັດສິນໃຈກ່ຽວກັບລັກສະນະແລະທິດທາງຂອງການ
+                  ສຶກສາໃນອະນາຄົດແລະການປະຕິບັດສິລະປະຂອງເຈົ້າ.</p>
+              </v-col>
            
-           <h2 class="white--text mb-1" style="font-weight: 500">
-             Foundation of the Deaf
-           </h2>
-      
-       </v-card>
-       
-                   
-       
-                   
- 
-    </div>
-            
-                    
-            
-
-            </v-container>
+            </v-row>
+           
+            <v-divider class="mt-10 " />
+          </div>
+          <div>
+            <v-card
+              tile
+              class="d-inline-flex mx-auto px-15 "
+              color="#297FCA"
+            >
+              <h1 class="white--text mb-1" style="font-weight: 500">
+                ພວກເຮົາເຮັດຫຍັງແດ່?
+              </h1>
+            </v-card>
+          </div>
+          <div class="pl-15 pr-15 pt-10  ma-10 txtp" style="color:#5B5B5B">
+            <p >•  &nbsp;&nbsp;&nbsp; ມອບລາງວັນສົ່ງເສີມການສຶກສາ ເພື່ອສະໜັບສະໜູນຄວາມມຸ່ງມາດປາດຖະໜາດ້ານການສຶກສາຂອງນັກຮຽນມັດທະຍົມຕອນປາຍ ແລະ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ມັດທະຍົມສຶກສາທົ່ວປະເທດ.</p>
+            <p>•  &nbsp;&nbsp;&nbsp; ການຄົ້ນຄວ້າກອງທຶນ, ໂດຍສະເພາະໂດຍຜ່ານ Peter Howson Research Fellowship, Education Research &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fellowship ແລະ PhD Scholarshi ໂຄງການຄົ້ນຄວ້າທົດລອງແລະການຄົ້ນຄວ້າການສຶກສາ.</p>
+            <p>•  &nbsp;&nbsp;&nbsp; ສະໜອງຂໍ້ມູນໃຫ້ແກ່ປະຊາຊົນທົ່ວໄປ ແລະ ຜູ້ທີ່ຫູໜວກ ຫຼື ຫູໜວກ ຢູ່ໃນຂອບເຂດກ້ວາງຂອງບັນຫາ ແລະ ຫົວຂໍ້ຕ່າງໆ.</p>
+            <p>•  &nbsp;&nbsp;&nbsp; ໃນ​ຖາ​ນະ​ເປັນ​ອົງ​ການ​ນໍາ​ພາ​, ດໍາ​ເນີນ​ການ​ແລະ​ປະ​ສານ​ງານ​ກິດ​ຈະ​ກໍາ​ໃນ​ໄລ​ຍະ​ອາ​ທິດ​ການ​ຮັບ​ຮູ້​ການ​ໄດ້​ຍິນ​ເພື່ອ​ຍົກ​ສູງ​ຄວາມ​ຮູ້​ຂອງ​ຄົນ​ຫູ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;​ຫນວກ ​ແລະ ​ການ​ສູນ​ເສຍ​ການ​ໄດ້​ຍິນ​.</p>
+            <p>•  &nbsp;&nbsp;&nbsp; ສະໜອງທຶນຊ່ວຍເຫຼືອລ້າໃຫ້ແກ່ອົງການຕ່າງໆສຳລັບໂຄງການທີ່ສະໜັບສະໜູນພາລະກິດຂອງພວກເຮົາ ແລະ ເປັນປະໂຫຍດແກ່ຄົນຫູໜວກ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ແລະ ຫູໜວກ.</p>
+            <p>•  &nbsp;&nbsp;&nbsp; ໃຫ້ຄວາມເປັນຜູ້ນໍາພາໃນບັນຫາທີ່ເກີດຈາກການໄດ້ຍິນສຽງລົບກວນ.</p>
+            <p>•  &nbsp;&nbsp;&nbsp; ມີສ່ວນຮ່ວມໃນການລະດົມທຶນເພື່ອໃຫ້ສາມາດປະຕິບັດວຽກງານທີ່ພວກເຮົາເຮັດ.</p>
+            <v-divider class="mt-15 " />
+          </div>
+          <div>
+            <v-card
+              tile
+              class="d-inline-flex  mx-auto px-15 "
+              color="#297FCA"
+            >
+              <h1 class="white--text mb-1" style="font-weight: 500">
+                ຜູ້ບໍລິຫານ & ສະມາຊິກ
+              </h1>
+            </v-card>
+          </div>
+          <div class="pl-15 pr-15 pt-5 ma-5 text-center ">
+            <p style="color:#5B5B5B" class="txtp">FOD is proud to have appointed three Governors.  These are all members of the organisation who have provided excellent service to the Foundation and the former Foundation Trust over many years.</p>
+            <h2 class="pt-5">ຜູ້ບໍລິຫານ</h2>
         
-        </v-main>
+                <v-row  class="pl-15 ml-15 pt-5 pb-10 justify-center " style="color:#5B5B5B" >
+                <v-col cols="4" >
+                    <v-img max-width="150" src="../assets/person.png"></v-img>
+                    <p class="pr-15 pt-5 mr-10">ທ່ານ xxxxx xxxxxxxxxx</p>
+                </v-col>
+                <v-col cols="4">
+                    <v-img max-width="150" src="../assets/person.png"></v-img>
+                    <p class="pr-15 pt-5 mr-10">ທ່ານ xxxxx xxxxxxxxxx</p>
+                </v-col>
+                <v-col cols="4">
+                    <v-img max-width="150" src="../assets/person.png"></v-img>
+                    <p class="pr-15 pt-5 mr-10">ທ່ານ xxxxx xxxxxxxxxx</p>
+                </v-col>
+               
+            </v-row>
+            <h2>ສະມາຊິກ</h2>
+            <v-row  class="pl-15 ml-15 pt-5 pb-10 justify-center " style="color:#5B5B5B" >
+                <v-col cols="4" >
+                    <v-img max-width="150" src="../assets/person.png"></v-img>
+                    <p class="pr-15 pt-5 mr-10">ທ່ານ xxxxx xxxxxxxxxx</p>
+                </v-col>
+                <v-col cols="4">
+                    <v-img max-width="150" src="../assets/person.png"></v-img>
+                    <p class="pr-15 pt-5 mr-10">ທ່ານ xxxxx xxxxxxxxxx</p>
+                </v-col>
+                <v-col cols="4">
+                    <v-img max-width="150" src="../assets/person.png"></v-img>
+                    <p class="pr-15 pt-5 mr-10">ທ່ານ xxxxx xxxxxxxxxx</p>
+                </v-col>
+                <v-col cols="4">
+                    <v-img max-width="150" src="../assets/person.png"></v-img>
+                    <p class="pr-15 pt-5 mr-10">ທ່ານ xxxxx xxxxxxxxxx</p>
+                </v-col>
+                <v-col cols="4">
+                    <v-img max-width="150" src="../assets/person.png"></v-img>
+                    <p class="pr-15 pt-5 mr-10">ທ່ານ xxxxx xxxxxxxxxx</p>
+                </v-col>
+               
+            </v-row>
+            
+          </div>
+          
+        </v-container>
+      </v-main>
     </div>
-    </v-app>
-  
+  </v-app>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.txtp{
+    font-size: 20px;
+}
+h1{
+    font-size: 50px;
+   
+}
 </style>
